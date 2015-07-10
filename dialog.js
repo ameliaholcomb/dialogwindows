9fb755bba5d895de8f865354d349d1e91c172969
Amelia Holcomb <amelia.holcomb@gmail.com>
2015-07-10 14:43:59 -0400

    working drag functionality

--- dialog.js
+++ dialog.js
@@ -1,18 +1,18 @@
 angular.module('root', [])
-.controller('index', ['$scope', function($scope) {
-	$scope.dialogs = [
-		{
-			minimized: true,
-			top: '10px',
-			left: '10px',
-			width: '600px',
-			height: '800px',
-			template: 'experiment-dialog.html'
-		}
-	]
-}])
-
-  .directive('makeDialog', function() {
+  .controller('index', ['$scope', function($scope){
+    $scope.dialogs = [
+      {
+      minimized: false,
+      width: 200,
+      height: 300,
+      top: 0,
+      left: 0,
+      template: 'experiment-dialog.html'
+      }
+    ]
+  }])
+  
+  .directive('makeDialog', function($document) {
 	return {
 		restrict: 'E',
 		scope: {
@@ -20,21 +20,48 @@ angular.module('root', [])
 		},
 		templateUrl: 'dialog.html',
 		link: function(scope, element, attrs) {
-			
+		  
 		  //minimization
 		  scope.minimize = function() {
 		    scope.model.minimized = true;
-		  };
+		  }; 
 		  
 		  //jqlite elements
 		  var topBar = element.find('span');
 		  var dialog = element.find('div');
 		  
-		  //draggable
-		  topBar.on('mousedown', function() {
-		    dialog.css('border', 'solid purple')
-		  })
+		  //drag variables
+		  var t = scope.model.top;
+		  var l = scope.model.left;
+		  var offsetL = 0
+		  var offsetT = 0;
 		  
+		  //drag functions
+		  topBar.on('mousedown', function(event) {
+		    event.preventDefault();
+		    offsetL = event.screenX - l;
+		    offsetT = event.screenY - t;
+		    $document.on('mousemove', mousemove);
+		    $document.on('mouseup', mouseup);
+		  });
+		    
+		  function mousemove(event) {
+		    l = event.screenX - offsetL;
+		    t = event.screenY - offsetT;
+		    dialog.css({
+            	top: t + 'px',
+            	left:  l + 'px'
+        	});
+		  }
+		  
+		  function mouseup(event) {
+		    $document.off('mousemove', mousemove);
+		    $document.off('mouseup', mouseup);
+		    scope.model.top = t;
+		    scope.model.left = l;
+		    scope.$apply();
+		  } 
+
 		}
 	};
 });
